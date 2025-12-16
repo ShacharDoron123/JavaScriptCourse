a67db23437ddc31223eb564100c26507a31a9c6f
ShacharDoron123 <shachar08.doron@gmail.com>
2025-12-16 10:44:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+
+console.log("hi");
+
+window.alert("hi");
+
+document.getElementById("myh1").textContent = "hi";
+document.getElementById("myp").textContent = "i like pizza";
+
+//this is a comment
+
+/* 
+ this
+ is
+ also 
+ a 
+ comment
+*/
+
+// Variables
+
+let x = 123;
+console.log(x);
+
+let age = 17;
+console.log(age);
+
+console.log(`my age is ${age} years old`);
+console.log(typeof age);
+
+let myname = "shachar"
+console.log(` my name is ${myname}`);
+console.log(typeof myname);
+
+let online = true;
+let forsale = false;
+
+document.getElementById("myname").textContent = `my name is ${myname}`;
+document.getElementById("age").textContent = `my age is ${age}`;
+
+// Arithmetic operators
+
+let students = 30;
+console.log(students);
+
+students = students + 1;
+console.log(students);
+
+students = students - 1;
+students = students / 2;
+students = students * 2;
+students = students ** 2;
+students = students % 2;
+
+students = 30;
+
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students ** 2;
+students %= 2;
+students++;
+students--;
+
+// User input
+
+// Method 1: window prompt
+let userName = window.prompt("What is your name?");
+console.log(userName);
+
+// Method 2: input + button
+document.getElementById("mySubmit").onclick = function () {
+  userName = document.getElementById("username").value;
+  document.getElementById("usernameH1").textContent = `Hi! ${userName}`;
+};
+
