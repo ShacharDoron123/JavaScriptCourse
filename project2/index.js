2a782c35bed228a2c19a7ce92ce18417c08ba3d3
ShacharDoron123 <shachar08.doron@gmail.com>
2025-12-17 11:47:30 +0200

    project2 files

---
+++ project2/index.js
@@ -0,0 +1,21 @@
+const decreaseBth = document.getElementById("decreaseBth");
+const resetBtn = document.getElementById("resetBth");
+const increaseBtn = document.getElementById("increaseBth");
+const countLabel = document.getElementById("CountLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count
+}
+
+decreaseBth.onclick = function(){
+    count--;
+    countLabel.textContent = count
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
