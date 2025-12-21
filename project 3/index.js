4caebf1e8f05b9a2abf4cdef8f582cc25a8a7ee2
ShacharDoron123 <shachar08.doron@gmail.com>
2025-12-21 11:48:06 +0200

    add color

---
+++ project 3/index.js
@@ -0,0 +1,23 @@
+document.getElementById("red").onclick = function(){
+    document.body.style.backgroundColor = "red";
+}
+document.getElementById("blue").onclick = function(){
+    document.body.style.backgroundColor = "blue";
+}
+document.getElementById("green").onclick = function(){
+    document.body.style.backgroundColor = "green";
+}
+document.getElementById("ran").onclick = function() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+document.getElementById("Send").onclick = function(){
+    let r = document.getElementById("r").value;
+    let g = document.getElementById("g").value;
+    let b = document.getElementById("b").value;
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+}
